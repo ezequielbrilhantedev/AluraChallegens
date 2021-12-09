ca6b23c48ba00be68225eb8028d4b0be24bbebaa
Ezequiel Brilhante <ezequielrb7@gmail.com>
2021-12-09 17:34:50 -0300

    Inicio da implementação do menu de navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MenuNavComponent } from './components/menu-nav/menu-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuNavComponent
   ],
   imports: [
     BrowserModule,
