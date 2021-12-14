0968ee556edea89cad21bdb35266edde57c7f5e7
ezequielbrilhante <e.ribeiro.brilhante@accenture.com>
2021-12-14 09:42:13 -0300

    Campo para mudar a linguagem

--- src/app/components/body/titulo-proj/titulo-proj.component.ts
+++ src/app/components/body/titulo-proj/titulo-proj.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-titulo-proj',
   templateUrl: './titulo-proj.component.html',
-  styleUrls: ['./titulo-proj.component.scss']
+  styleUrls: ['./titulo-proj.component.scss'],
 })
 export class TituloProjComponent implements OnInit {
+  linguagens = ['JavaScript', 'Python', 'HTML', 'CSS', 'C', 'C++'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
